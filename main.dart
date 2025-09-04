226cfd293eafe148a808be85287b65a224affae0
Luan Conrado <luanconrado07@gmail.com>
2025-09-04 00:00:08 +0000

    Feat: if statement

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+void main (){
+    int idade = 17;
+        /*if(idade >= 18){
+            print('Pode entrar na balada');
+        }
+        print('Fim do programa');*/
+
+    if(idade < 18){
+        print('Fim do programa');
+        return;
+    }
+    print('Pode entrar na balada');
+}
